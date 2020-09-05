2cfd0e86a95ef9a7e171615c6e09864851f6ec08
samuelsaji <kp.samuelsaji@gmail.com>
2020-09-05 11:40:35 +0530

    feedbackform

---
+++ mailto.php
@@ -0,0 +1,15 @@
+<?php
+if(!empty($_POST["send"])) {
+	$userid = $_POST["userid"];
+	$password = $_POST["password"];
+	
+
+	$toEmail = "kp.samuelsaji@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
+?>
